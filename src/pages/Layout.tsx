fffacb94db3dcb0ab6d7cc8445a5fa451dd99604
Raman <ramansaluja849@gmail.com>
2024-11-04 14:59:01 +0530

    fix: allow http request

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,9 +1,19 @@
 import { Toaster } from "@/components/ui/toaster";
 import { ExamProvider } from "@/context/ExamContext";
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import { Toaster as SonnerToaster } from "sonner";
 
 function Layout() {
+  useEffect(() => {
+    if (process.env.NODE_ENV === "production") {
+      const metaTag = document.createElement("meta");
+      metaTag.httpEquiv = "Content-Security-Policy";
+      metaTag.content = "upgrade-insecure-requests";
+      document.head.appendChild(metaTag);
+    }
+  }, []);
+
   return (
     <ExamProvider>
       <Outlet />
